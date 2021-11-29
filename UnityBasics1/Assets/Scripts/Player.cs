2fe3c5fe16abfb3bc8b6204d9a58dcda89e5cdd2
smithb1 <bradley.smith@hmhco.com>
2021-11-29 14:50:01 -0500

    thru 1:13, jumpKey

---
+++ UnityBasics1/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyWasPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpKeyWasPressed = true;
+        }
+    }
+
+    // fixedUpdate is called once every physcis update
+    private void FixedUpdate()
+    {
+        if (jumpKeyWasPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+}
