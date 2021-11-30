2523a2c25ad9de2d7aac1a6c110e97f5ccb3d985
smithb1 <bradley.smith@hmhco.com>
2021-11-30 11:36:03 -0500

    Player, layer, physics

--- UnityBasics1/Assets/Scripts/Player.cs
+++ UnityBasics1/Assets/Scripts/Player.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
     private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,14 +23,23 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             jumpKeyWasPressed = true;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
     // fixedUpdate is called once every physcis update
     private void FixedUpdate()
     {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if(Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
         if (jumpKeyWasPressed)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
     }
